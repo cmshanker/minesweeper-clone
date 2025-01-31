fe7e4748c46749de3e31ca1d53f38ce88053c2f3
cshanker <cassandrashanker@gmail.com>
2025-01-31 15:29:33 -0500

    basic functionality for clearing spaces, placing flags, and clicking on mines done

--- scripts/mine_grid.gd
+++ scripts/mine_grid.gd
@@ -1,13 +1,15 @@
 extends TileMapLayer
 
-@export var rows = 9
-@export var columns = 9
-@export var mine_count = 10
-@export var mine_locations = []
+@export var rows: int = 9
+@export var columns: int = 9
+@export var mine_count: int = 10
+@export var mine_locations: Array[Vector2i] = []
 
 @onready var mine_grid: TileMapLayer = $"."
 @onready var first_click: bool = true
+@onready var visited_cells: Array[Vector2i] = []
 
+# dictionary mapping cell tile types to their atlas coords in the tilemap
 const CELLS = {
 	'SQUARE': Vector2i(0, 0),
 	'PRESSED': Vector2i(1, 0),
@@ -26,32 +28,34 @@ const CELLS = {
 	'7': Vector2i(6, 1),
 	'8': Vector2i(7, 1),
 }
-const TILE_SET_ID = 1
+const TILE_SET_ID: int = 1
 
 # helper function to set a cell to a particular cell type
-func set_tile_cell(cell_coord: Vector2i, cell_type: String):
+func set_tile_cell(cell_coord: Vector2i, cell_type: String) -> void:
 	set_cell(cell_coord, TILE_SET_ID, CELLS[cell_type])
 
 # helper function to get the 3x3 area around a cell, excluding any that are
-# outside of the bounds of the grid
+# outside of the bounds of the grid and excluding the center
 func get_neighbors(loc: Vector2i) -> Array[Vector2i]:
 
 	var neighboring_cells: Array[Vector2i] = []
 	for i in range(loc[0] - 1, loc[0] + 2):
 		for j in range(loc[1] - 1, loc[1] + 2):
+			if Vector2i(i, j) == loc:
+				continue
 			if i >= 0 && j >= 0 && i < rows && j < columns:
 				neighboring_cells.push_back(Vector2i(i, j))
 
 	return neighboring_cells
 
-func _ready():
+func _ready() -> void:
 	# set all cells in grid to the non-pressed square tile
 	for i in rows:
 		for j in columns:
 			var cell_coord = Vector2i(i, j)
 			set_tile_cell(cell_coord, 'SQUARE')
 
-func _input(event: InputEvent):
+func _input(event: InputEvent) -> void:
 	
 	if event is InputEventMouseButton:
 		var click_location = mine_grid.local_to_map(event.position)
@@ -68,20 +72,26 @@ func handle_left_click(loc: Vector2i) -> void:
 	if first_click:
 		first_click = false
 		set_initial_mines(loc)
-		var neighboring_cells = get_neighbors(loc)
-		for cell in neighboring_cells:
-			var neighboring_mines = check_neighboring_mines(cell)
-			match neighboring_mines:
-				0:
-					set_tile_cell(cell, 'PRESSED')
-				_:
-					set_tile_cell(cell, '%s' % neighboring_mines)
+	
+	# if we clicked on an uncleared tile, run through our logic for clearing
+	# cells, or clicking on a mine
+	if mine_grid.get_cell_atlas_coords(loc) == Vector2i(0, 0):
+		if mine_locations.find(loc) != -1:
+			click_mine(loc)
+		else:
+			clear_cells(loc)
 					
 func handle_right_click(loc: Vector2i) -> void:
-	print(loc)
+	# if right click on uncleared square, set cell to flag
+	# if right click on flag, set back to uncleared square
+	# otherwise do nothing
+	if mine_grid.get_cell_atlas_coords(loc) == Vector2i(0, 0):
+		set_tile_cell(loc, 'FLAG')
+	elif mine_grid.get_cell_atlas_coords(loc) == Vector2i(2, 0):
+		set_tile_cell(loc, 'SQUARE')
 
 # sets the initial locations of the mines in the grid
-func set_initial_mines(first_click_loc: Vector2i):
+func set_initial_mines(first_click_loc: Vector2i) -> void:
 	# we don't want to place any mines in the 3x3 area around the first click
 	var neighboring_cells = get_neighbors(first_click_loc)
 	while mine_locations.size() < mine_count:
@@ -89,6 +99,27 @@ func set_initial_mines(first_click_loc: Vector2i):
 		if neighboring_cells.find(new_mine_location) == -1 && \
 			mine_locations.find(new_mine_location) == -1:
 			mine_locations.push_back(new_mine_location)
+			
+# clear cells, if a cell has no neighboring mines we can clear all of its
+# adjacent cells as well, and continue clearing cells that way recursively
+func clear_cells(loc: Vector2i) -> void:
+	visited_cells.push_back(loc)
+	var neighboring_mines = check_neighboring_mines(loc)
+	if neighboring_mines == 0:
+		set_tile_cell(loc, 'PRESSED')
+		var neighboring_cells = get_neighbors(loc)
+		for cell in neighboring_cells:
+			if visited_cells.find(cell) == -1:
+				clear_cells(cell)
+	else:
+		set_tile_cell(loc, '%s' % neighboring_mines)
+		
+# end the current game if a user clicks on a mine		
+func click_mine(loc: Vector2i) -> void:
+	set_tile_cell(loc, 'MINE_PRESSED')
+	for mine in mine_locations:
+		if mine != loc:
+			set_tile_cell(mine, 'MINE')
 
 # returns the amound of neighboring mines to the location clicked		
 func check_neighboring_mines(loc: Vector2i) -> int:
